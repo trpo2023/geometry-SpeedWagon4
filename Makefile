2ed72ae8736db024804a1cde0ede8b5e8d4da90a
Dmitrii Oreshkin <hachirokuiiiae86@gmail.com>
2023-04-05 14:58:36 +0700

    Finish LibGeometry

--- Makefile
+++ Makefile
@@ -1,10 +1,37 @@
-CC = gcc
-CFLAGS = -Wall -Werror
+CC := g++
+CFLAGS := -Wall -Werror
 
-main: main.o
-	$(CC) $(CFLAGS) main.c -o main
+SRC_DIR := src
+OUT_DIR := bin
+OBJ_DIR := obj
 
-test:
-	./main test.txt
+MAIN_SRCS := $(wildcard $(SRC_DIR)/geometry/*.c)
+MAIN_OBJ := $(MAIN_SRCS:$(SRC_DIR)/geometry/%.c=$(OBJ_DIR)/geometry/%.o)
 
-.PHONY: test
+LIB_SRCS := $(wildcard $(SRC_DIR)/libgeometry/*.c)
+LIB_OBJ := $(LIB_SRCS:$(SRC_DIR)/libgeometry/%.c=$(OBJ_DIR)/libgeometry/%.o)
+
+LIB_TARGET := $(OUT_DIR)/libgeometry.a
+TARGET := $(OUT_DIR)/main
+
+.PHONY: all clean test
+
+all: $(LIB_TARGET) $(TARGET)
+
+$(TARGET): $(MAIN_OBJ) 
+	$(CC) $(CFLAGS) $^ -lm -L$(OUT_DIR) -lgeometry -o $@
+
+$(OBJ_DIR)/geometry/%.o: $(SRC_DIR)/geometry/%.c
+	$(CC) $(CFLAGS) -c -Isrc $< -o $@
+
+$(LIB_TARGET): $(LIB_OBJ)
+	ar rcs $@ $^
+
+$(OBJ_DIR)/libgeometry/%.o: $(SRC_DIR)/libgeometry/%.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+run:
+	./$(OUT_DIR)/main test.txt
+
+clean:
+	rm -rf $(OBJ_DIR)/* $(TARGET)
---
+++ src/libgeometry/parser.c
@@ -0,0 +1,36 @@
+#include "parser.h"
+#include "stdio.h"
+#include "math.h"
+
+void print_error(int col, const char* line, const char* message) {
+    printf("%s\n", line);
+    for(int i = 0; i < col; i++)
+    {
+        printf(" ");
+    }
+    printf("^ Error at colusmn %d: %s\n", col, message);
+}
+
+int parse_circle(const char* line, struct Circle* circle) {
+    int count = sscanf(line, "circle(%lf%*[ \t]%lf,%lf)", &circle->x, &circle->y, &circle->radius);
+    if (count == 3) {
+        return 1;
+    } else {
+        print_error(0, line, "invalid circle format.");
+        return 0;
+    }
+}
+
+double get_circle_area(struct Circle* circle) {
+    return M_PI * circle->radius * circle->radius;
+}
+
+double get_circle_perimeter(struct Circle* circle) {
+    return 2 * M_PI * circle->radius;
+}
+
+int circles_intersect(struct Circle* circle1, struct Circle* circle2) {
+    double distance = sqrt(pow(circle2->x - circle1->x, 2) + pow(circle2->y - circle1->y, 2));
+    double radius_sum = circle1->radius + circle2->radius;
+    return distance <= radius_sum;
+}
